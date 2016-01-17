537509d319a889ecbba839f135b9ddf4039b6d9c
Stephen Moore <stephen@delfick.com>
2016-01-17 12:56:15 +1100

    Fixing the scheduler

--- python_dashing/scheduler.py
+++ python_dashing/scheduler.py
@@ -16,19 +16,19 @@ class Scheduler(object):
     def run(self, datastore, force=False):
         now = datetime.datetime.now()
         for cron, func, module, module_name in self.checks:
-            key = "{0}_{1}".format(cron.replace(" ", "_").replace("/", "SLSH").replace("*", "STR"), func.__name__)
-            iterable = croniter(cron, self.check_times.get(key, now))
+            cron_key = "{0}_{1}".format(cron.replace(" ", "_").replace("/", "SLSH").replace("*", "STR"), func.__name__)
+            iterable = croniter(cron, self.check_times.get(cron_key, now))
             nxt = iterable.get_next(datetime.datetime)
             if not force:
-                if nxt > now and key in self.check_times:
+                if nxt > now and cron_key in self.check_times:
                     continue
 
             log.info("Triggering cron: {0}.{1} '{2}'".format(module_name, func.__name__, cron))
             try:
                 ds = datastore.prefixed("{0}-{1}".format(module.relative_to, module_name))
-                for key, value in func(now - self.check_times.get(key, now)):
+                for key, value in func(now - self.check_times.get(cron_key, now)):
                     ds.create(key, value)
             except Exception:
                 log.exception("Error running a check\tmodule={0}\tcheck={1}".format(module_name, func.__name__))
             finally:
-                self.check_times[key] = now
+                self.check_times[cron_key] = now
